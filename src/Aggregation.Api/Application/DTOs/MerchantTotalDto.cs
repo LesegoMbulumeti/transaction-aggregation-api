c09424e45d90ea5e61ae0907b3aa0fda169b4a1c
lesegombulumeti <lesegombulumeti2@gmail.com>
2026-09-08 08:19:33 +0200

    I added the controllor for aggregations

---
+++ src/Aggregation.Api/Application/DTOs/MerchantTotalDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Aggregation.Api.Application.DTOs;
+
+public class MerchantTotalDto
+{
+    public string Description { get; set; } = string.Empty;
+    public int TransactionCount { get; set; }
+    public decimal TotalAmount { get; set; }
+}
